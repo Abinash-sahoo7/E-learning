0a132715ccc5d82f074347f81ba20e20d7bf9885
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2024-12-08 21:24:15 +0530

    project upload

---
+++ src/components/Common/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <div className="bg-richblack-800">
+            <div className="mx-auto flex w-11/12 max-w-maxContent flex-row items-center justify-between p-7 text-sm text-richblack-400">
+                <div className="w-full text-justify text-lg md:text-center">
+                    <strong> Copyright © 2024 E-Learning | </strong> Made with
+                    ❤️ Abinash Ashutosh Aditya
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
